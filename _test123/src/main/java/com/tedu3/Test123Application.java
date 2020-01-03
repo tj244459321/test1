d93df21fbba8e3d37ca67316e9cbffc4e7b34dbf
tj244459321 <59460275+tj244459321@users.noreply.github.com>
2020-01-03 10:59:07 +0800

    Update Test123Application.java

--- _test123/src/main/java/com/tedu3/Test123Application.java
+++ _test123/src/main/java/com/tedu3/Test123Application.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test123Application {
-
+       String a = "123";
 	public static void main(String[] args) {
+
 		SpringApplication.run(Test123Application.class, args);
 	}
 
