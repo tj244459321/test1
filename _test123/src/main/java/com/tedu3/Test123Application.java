43134872d595947432f0f978b375c32e3d064579
Administrator <Administrator@USER-20161013UJ>
2020-01-03 10:56:01 +0800

    123

---
+++ _test123/src/main/java/com/tedu3/Test123Application.java
@@ -0,0 +1,13 @@
+package com.tedu3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test123Application.class, args);
+	}
+
+}
